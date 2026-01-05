65a4da66d45c75dc79d7614487e7229281acf252
Rui Araújo <ruiaraujo012@gmail.com>
2026-01-05 17:02:16 +0000

    refactor(setup): some configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import logo from '@/assets/white-logo.png';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+const App = () => (
+  <>
+    <div className='h-screen w-screen bg-linear-to-br from-tanigma-rose to-60% to-tanigma-sky'>
+      <div className='flex h-screen justify-center items-center'>
+        <img
+          alt='Logo'
+          className='text-center w-[50%]'
+          src={logo}
+        />
       </div>
-    </>
-  );
-}
+
+      <h1 className='text-tanigma-rose'>Tanigma</h1>
+    </div>
+  </>
+);
 
 export default App;
