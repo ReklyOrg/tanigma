d7a779cb9799787f998bb2673bb3f023c08df38f
Rui Araújo <ruiaraujo012@gmail.com>
2026-01-05 14:26:03 +0000

    feat(setup): add tailwind and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import eslintPlugin from '@nabla/vite-plugin-eslint';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 import { checker } from 'vite-plugin-checker';
@@ -34,6 +35,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       react(),
+      tailwindcss(),
       checker({
         overlay: false,
         terminal: true,
