fca709c7c4f2cc8ae3a634f9d1ee396a74585137
Rui Araújo <ruiaraujo012@gmail.com>
2026-01-08 19:22:57 +0000

    feat(build): add docker and some other build configs

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,21 @@
-const Footer = () => (
-  <div className='bg-tanigma-mauve min-h-60'>
-    <h2 className='text-tanigma-text-inverse font-bold text-xl'>Rodapé</h2>
-  </div>
-);
+import { useMemo } from 'react';
+
+const Footer = () => {
+  const buildDate = useMemo(() => {
+    try {
+      return new Date(APP_VERSION_DATE).toLocaleString();
+    } catch {
+      return '';
+    }
+  }, []);
+
+  return (
+    <div className='bg-tanigma-mauve min-h-60 flex flex-col justify-between'>
+      <h2 className='text-tanigma-text-inverse font-bold text-xl'>Rodapé</h2>
+
+      <p className='text-center text-white'>Última atualização em: {buildDate}</p>
+    </div>
+  );
+};
 
 export default Footer;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,20 @@
 /* eslint-disable unicorn/prevent-abbreviations */
 /// <reference types="vite/client" />
+
+// interface ViteTypeOptions {
+//   // By adding this line, you can make the type of ImportMetaEnv strict
+//   // to disallow unknown keys.
+//   strictImportMetaEnv: unknown;
+// }
+
+// interface ImportMetaEnv {
+//   readonly SOME_ENV: string;
+// }
+
+// interface ImportMeta {
+//   readonly env: ImportMetaEnv;
+// }
+
+declare const APP_ENVIRONMENT: string;
+declare const APP_VERSION: string;
+declare const APP_VERSION_DATE: string;
